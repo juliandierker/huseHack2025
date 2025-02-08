c9b5bd1ef14cf06f3dcafbd2d82d943ae848e8ba
Henning Husemann <78979296+henninghusemann@users.noreply.github.com>
2025-02-08 23:31:21 +0100

    fixed file path to be relative

--- helper/solution_store_helper.py
+++ helper/solution_store_helper.py
@@ -11,7 +11,7 @@ def store_solution(solver_name,problem_file_path,solution_path,solution_length,c
 
     with open(file_path,"w+") as file:
         file.write(solver_name + ',')
-        file.write(problem_file_path + ',')
+        file.write("./" + problem_file_path + ',')
         file.write(str(computation_time) + ',')
         file.write(str(solution_length) + '\n')
         file.write(",".join(map(str,solution_path)))

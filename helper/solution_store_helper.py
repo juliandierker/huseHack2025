9a7c1e751711594a3fdaaf5e98dc068de6caebdb
Henning Husemann <78979296+henninghusemann@users.noreply.github.com>
2025-02-08 22:51:03 +0100

    updated solution store helper

--- helper/solution_store_helper.py
+++ helper/solution_store_helper.py
@@ -4,13 +4,14 @@ import time
 def get_timestamp():
     return time.strftime("%H%M%S")
 
-def store_solution(solver_name,problem_name,solution_path,computation_time):
+def store_solution(solver_name,problem_file_path,solution_path,solution_length,computation_time):
     timestamp = get_timestamp()
     
     file_path = "solutions/solution_" + solver_name + "_" + timestamp + ".csv"
 
     with open(file_path,"w+") as file:
-        file.write(solver_name + '\n')
-        file.write(problem_name + '\n')
-        file.write(str(computation_time) + '\n')
+        file.write(solver_name + ',')
+        file.write(problem_file_path + ',')
+        file.write(str(computation_time) + ',')
+        file.write(str(solution_length) + '\n')
         file.write(",".join(map(str,solution_path)))

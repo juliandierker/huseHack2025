5e95084aa263fa56254c46b3df520f75a5f957e3
Henning Husemann <78979296+henninghusemann@users.noreply.github.com>
2025-02-08 23:25:03 +0100

    fixed benchmark runner

--- benchmark.py
+++ benchmark.py
@@ -1,20 +1,26 @@
 import subprocess
 import os 
 
-problem_path = "problems/"
 solver_path = "solver/"
+problem_path = "problems/"
 
+# load all solvers
 all_solver_filenames = os.listdir(solver_path)
-all_problem_filenames = os.listdir(problem_path)
+all_solver_paths = [os.path.join(solver_path,solver_filename) for solver_filename in all_solver_filenames]
+
+print("all solver filenames:",all_solver_filenames)
+print("all solver pathes:",all_solver_paths)
 
 # load all problems
-all_problems = [os.path.join(problem_path,problem_filepath) for problem_filepath in os.listdir(all_problem_filenames)]
+all_problem_filenames = os.listdir(problem_path)
+all_problem_paths = [os.path.join(problem_path,problem_filename) for problem_filename in all_problem_filenames]
+
+print("all problem filenames:",all_problem_filenames)
+print("all problem file paths",all_problem_paths)
 
 # execute all solvers on all problems
-for solver_filename in all_solver_filenames:
-    print("[BENCHMARK] running solver: ",solver_filename)
-    solver_path = os.path.join(solver_path,solver_filename)
-    for idx,problem_filename in enumerate(all_problem_filenames):
+for solver_path in all_solver_paths:
+    print("[BENCHMARK] running solver: ",solver_path)
+    for idx,problem_path in enumerate(all_problem_paths):
         print("[BENCHMARK] problem",idx+1,"/",len(all_problem_filenames))
-        problem_path = os.path.join(problem_path,problem_filename)
         subprocess.run(["python",solver_path,problem_path])
